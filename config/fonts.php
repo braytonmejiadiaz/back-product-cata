35693bc3bfc591effc03f7b23d896db6508a8a88
braytonmejiadiaz <brayton.mejia13@gmail.com>
2025-04-29 18:00:15 -0500

    add new fonts

--- config/fonts.php
+++ config/fonts.php
@@ -23,6 +23,26 @@ return [
         'Poppins' => [
             'family' => 'Poppins, sans-serif',
             'url' => 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap'
+        ],
+        'Inter' => [
+            'family' => 'Inter, sans-serif',
+            'url' => 'https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap'
+        ],
+        'Nunito' => [
+            'family' => 'Nunito, sans-serif',
+            'url' => 'https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap'
+        ],
+        'Playfair Display' => [
+            'family' => 'Playfair Display, serif',
+            'url' => 'https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap'
+        ],
+        'Merriweather' => [
+            'family' => 'Merriweather, serif',
+            'url' => 'https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&display=swap'
+        ],
+        'Source Sans Pro' => [
+            'family' => 'Source Sans Pro, sans-serif',
+            'url' => 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap'
         ]
     ],
 
